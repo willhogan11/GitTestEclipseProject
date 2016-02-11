0998581753d52ca15afe9b8d2fc5a598f4459010
Will Hogan <willhogan11@hotmail.com>
2016-02-11 22:36:22 +0000

    First Commit

---
+++ GitTestEclipseProject/src/Runner.java
@@ -0,0 +1,9 @@
+public class Runner {
+
+	public static void main(String[] args) {
+		int[] num = {1,2,3,4,5,6,7,8,9,10};
+		
+		for(int n : num)
+			System.out.println("Numbers in Array are: " + n);
+	}
+}
